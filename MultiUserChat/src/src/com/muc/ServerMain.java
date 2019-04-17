dcf85d75ed701f305ded584225d6a66f8cc97922
jtran4481 <jtran4481@westada.org>
2019-04-17 12:25:47 -0600

    Incomplete, initial

---
+++ MultiUserChat/src/src/com/muc/ServerMain.java
@@ -0,0 +1,25 @@
+package com.muc;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        int port = 8818;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while(true) {
+
+                Socket clientSocket = serverSocket.accept();
+                OutputStream outputStream = clientSocket.getOutputStream();
+                outputStream.write("hello world\n".getBytes());
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
