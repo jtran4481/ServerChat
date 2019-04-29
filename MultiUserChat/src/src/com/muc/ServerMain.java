fee7b6f470ca4e7e4bf2845101219544427d7f64
jtran4481 <jtran4481@westada.org>
2019-04-29 12:16:43 -0600

    Initial commit

--- MultiUserChat/src/src/com/muc/ServerMain.java
+++ MultiUserChat/src/src/com/muc/ServerMain.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class ServerMain {
     public static void main(String[] args) {
@@ -15,9 +16,15 @@ public class ServerMain {
                 Socket clientSocket = serverSocket.accept();
                 OutputStream outputStream = clientSocket.getOutputStream();
                 outputStream.write("hello world\n".getBytes());
+                for (int i=0; i<10; i++) {
+                    outputStream.write(("Time now is " + new Date() + "\n").getBytes());
+                    Thread.sleep(1000);
+                }
                 clientSocket.close();
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch(InterruptedException e) {
             e.printStackTrace();
         }
     }
